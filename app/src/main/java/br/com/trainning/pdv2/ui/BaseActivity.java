3f8b6b1f4e6703c0ebc62609c55943bf53bbbe0f
android <leoonardogomes17@gmail.com>
2016-03-05 12:32:26 -0300

    .

--- app/src/main/java/br/com/trainning/pdv2/ui/BaseActivity.java
+++ app/src/main/java/br/com/trainning/pdv2/ui/BaseActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import butterknife.ButterKnife;
 
 public class BaseActivity extends AppCompatActivity {
+
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
